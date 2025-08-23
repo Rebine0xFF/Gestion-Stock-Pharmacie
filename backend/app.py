81a5562f96a5a80ab23551364357caed5819c1a8
Rebine0xFF <hrobin.67000@gmail.com>
2025-08-23 15:10:15 +0200

    Refactor app.py to integrate database table reading function and add .gitignore

--- backend/app.py
+++ backend/app.py
@@ -10,9 +10,11 @@ app.secret_key = "Y76RGT67-èEF676GEeg56"
 def index():
     return "Message d'accueil"
 
-def remove_item():
-
+def read_db_table():
+    table = (newdb.read_table())
+    print(table)
 
 
 if __name__ == "__main__":
+    read_db_table()
     app.run(host="0.0.0.0", port=80)
