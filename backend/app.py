7ba580c060e4359433b48f03c630d1d5215706b7
Rebine0xFF <hrobin.67000@gmail.com>
2025-08-23 15:12:16 +0200

    Clean up

--- backend/app.py
+++ backend/app.py
@@ -6,13 +6,20 @@ import newdb
 app = Flask(__name__)
 app.secret_key = "Y76RGT67-èEF676GEeg56"
 
-@app.route("/items")
+
+
+
+
+@app.route("/")
 def index():
     return "Message d'accueil"
 
 def read_db_table():
     table = (newdb.read_table())
     print(table)
+
+
+
 
 
 if __name__ == "__main__":
